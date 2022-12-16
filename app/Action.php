3b77b16a0aefeb61d96d0dac6eebdc3d18ed3638
Etienne Pierrot <etienne.pierrot@gmail.com>
2022-12-17 00:37:19 +0100

    remove requirement of user_id

--- app/Action.php
+++ app/Action.php
@@ -24,7 +24,7 @@ class Action extends Model
 
     protected $rules = [
         'name'     => 'required|min:5',
-        'user_id'  => 'required|exists:users,id',
+        // 'user_id'  => 'required|exists:users,id',
         'group_id' => 'required|exists:groups,id',
         'start'    => 'required',
         'stop'     => 'required',
