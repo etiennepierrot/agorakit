3f7841d589218eb5c4d79c6ffcb7dbc079418f46
Etienne Pierrot <etienne.pierrot@gmail.com>
2022-12-17 00:30:10 +0100

    add log

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -136,25 +136,26 @@ class ActionController extends Controller
         ->where('ga_id', '<>', null);
 
         foreach ($groups as $group) {
-            echo $group->ga_id . "\n";
             $response = Http::get('https://actionpopulaire.fr/api/groupes/'.$group->ga_id.'/evenements/a-venir/');
             $json = json_decode($response->body(), true);
             foreach($json as $action_pop) {
-                $action_pop_id = $action_pop['id'];
-                echo $action_pop_id . "\n";
                 $action = \App\Action::firstOrNew(['external_ref' => $action_pop['id']]);
+                echo $action->exists();
                 $action->name = $action_pop['name'];
-                    $action->body = $action_pop['name'];
-                    $action->start = \Carbon\Carbon::parse($action_pop['startTime']);
-                    $action->stop = \Carbon\Carbon::parse($action_pop['endTime']);
-                    $action->location = $action_pop['location']['shortAddress'];
-                    $action->latitude = $action_pop['location']['coordinates']["coordinates"][1];
-                    $action->longitude = $action_pop['location']['coordinates']["coordinates"][0];
-                    $action->user_id = 1;
-                    $action->group_id = $group->id;
-                    echo $action;
-                    $action->save();
-                
+                $action->body = $action_pop['name'];
+                $action->start = \Carbon\Carbon::parse($action_pop['startTime']);
+                $action->stop = \Carbon\Carbon::parse($action_pop['endTime']);
+                $action->location = $action_pop['location']['shortAddress'];
+                $action->latitude = $action_pop['location']['coordinates']["coordinates"][1];
+                $action->longitude = $action_pop['location']['coordinates']["coordinates"][0];
+                $action->user_id = 1;
+                $action->group_id = $group->id;
+                $action->save();
+                if ($action->isInvalid()) {
+                    echo $action->getErrors();
+                } else {
+                    echo 'ACTION has been created/updated in db';
+                }
             }
         }
     }
